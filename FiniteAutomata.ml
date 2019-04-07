52a9a517fa9b6b829d74495668f047e9512c0d77
David <david.pinguim2@hotmail.com>
2019-04-07 21:13:50 +0100

    Funciona ate ao productive, não inclusive

--- FiniteAutomata.ml
+++ FiniteAutomata.ml
@@ -78,32 +78,69 @@ let flatMap f l =
     List.flatten (List.map f l)
 ;;
 
-let rec getAlphabetList t=
-    match t with
+let rec print_list l=
+	match l with
+	[] ->()
+	| x::xs -> print_string x; print_string "\n";print_list xs
+;;
+
+let rec print_list_list ll=
+	match ll with
+	[]->();
+	| x::xs -> print_list x; print_string "Next List\n"; print_list_list xs
+;;
+
+let rec getAlphabetList ts= (*Creates a list of the symbols of every state. With repeats and not sorted*)
+    match ts with
     []-> []
     | x::xs -> 
     	match x with
     	 (_,s,_)-> s::(getAlphabetList xs)
 ;;
 
-let rec getStateList t=
-    match t with
+let rec getStateList ts= (*Creates a list of the states of an Automata. Regardless of repeats and order*)
+    match ts with
     []-> []
     | x::xs -> 
     	match x with
     	 (s1,_,s2)-> [s1;s2]@(getStateList xs)
 ;;
 
+let rec isTransitionResultDeterministic t ts=(*Evaluates if a transition t is deterministic*)
+	match t with
+	(s1,sy,s2) -> match ts with
+					[]-> true
+					| x::xs -> match x with
+								(s3,sy2,s4)-> if (s1=s3)&&(sy=sy2)&&(s2<>s4) then false else isTransitionResultDeterministic t xs
+;;
+
+let rec isDeterministic ts= (*Evaluates if a fa is deterministic*)
+	match ts with
+	[] -> true
+	| x::xs -> if isTransitionResultDeterministic x ts then isDeterministic xs else false
+;;
+
+let rec statesReachableFrom s ts= (*Returns a list of the states that are reachable from the state s*)
+	match ts with 
+	[] -> []
+	| x::xs -> match x with
+				(s1,sy,s2) -> if s1=s then s2::(statesReachableFrom s xs) else statesReachableFrom s xs
+;;
+
+let rec stateListContainsAcceptedState a_s sl= (*Evaluates if the state list sl contains at least one accepted state*)
+		match a_s with
+		[] -> false;
+		| x::xs -> if List.mem x sl then (print_string "Sim, contem\n";true) else (print_string "Falso, nao contem\n";stateListContainsAcceptedState xs sl;)
+;;
+
 (* PUBLIC FUNCTIONS *)
 let getAlphabet fa =
-	match fa with
-     {initialState=i; transitions=t; acceptStates=a}-> canonical(getAlphabetList t)
+	canonical(getAlphabetList fa.transitions)
 ;;
 
 
 let getStates fa =
-	match fa with
-    {initialState=i; transitions=t; acceptStates=a} -> canonical (getStateList t)
+	canonical (getStateList fa.transitions)
 ;;
 
 let gcut s ts =
@@ -114,19 +151,42 @@ let gcut s ts =
 ;;
 
 let determinism fa =
-    false
+    isDeterministic fa.transitions
 ;;
 
 let reachable fa =
-    canonical []
+	canonical (statesReachableFrom fa.initialState (canonical fa.transitions))
 ;;
 
-let productive fa =
-    canonical []
+let productive fa =(*Doesn't work*)
+	let rec getReachableStateList sl= (*Returns a list of lists of reachable states for each state in sl*)
+		match sl with
+		[] -> []
+		| x::xs -> (statesReachableFrom x fa.transitions)::(getReachableStateList xs) in
+
+	let containsAcceptedState sl= (*Function to be called in Map to generate a list of states who are productive*)
+		stateListContainsAcceptedState fa.acceptStates sl in 
+
+	let rec productiveStateList bool_l sl= (*Generates a list of the productive states*)
+		match bool_l with
+		[] -> []
+		| x::xs -> match sl with y::ys -> if x=true then y::(productiveStateList xs ys) else productiveStateList xs ys in
+
+	print_list_list (getReachableStateList (getStates fa));
+	if (determinism fa) then (getStates fa) else productiveStateList (List.map containsAcceptedState (getReachableStateList (getStates fa))) (getStates fa)
 ;;
 
-let accept w fa =
-    false
+let accept w fa = (*Works but can replace thos fa.transitions with smaller lists*)
+
+	let rec nextState currentSymbol currentState ts=
+		match ts with
+		[] -> fa.initialState
+		| x::xs -> match x with (s1,sy,s2) -> if (s1=currentState && sy=currentSymbol) then s2 else nextState currentSymbol currentState xs in 
+    let rec stateMachine wrd currentState=
+    	match wrd with
+    	[] -> false
+    	| x::xs -> if (List.mem (nextState x currentState fa.transitions) fa.acceptStates) then true else stateMachine xs (nextState x currentState fa.transitions) in 
+    stateMachine w fa.initialState
 ;;
 
 let generate n fa =
@@ -136,7 +196,3 @@ let generate n fa =
 let accept2 w fa =
     false
 ;;
-
-(*TESTES*)
-getAlphabet abcND;;
-
